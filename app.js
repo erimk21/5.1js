0dc8d3dcea1cceb7f2179cfe7dbc1b731688abf5
erimk21 <9019259@student.zadkine.nl>
2022-10-13 13:15:17 +0200

    opzet 5.1

---
+++ app.js
@@ -0,0 +1,12 @@
+
+alert("hello wereld");
+
+var getal1 = 12;
+var getal2 = 4;
+var som = getal1 * getal2;
+
+var kleur = prompt("blue");
+
+document.getElementById("kop").style.color= kleur;
+ 
+document.getElementById("kop").innerHTML="java is niet cool";
